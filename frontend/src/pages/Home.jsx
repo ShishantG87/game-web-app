273abbe437dbc13067d933810527fdc10a01e15b
ShishantG87 <shishantbohora44@gmail.com>
2025-11-29 17:37:56 -0500

    added login function

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">You are logged in!</h1>
+      <p className="text-xl">Welcome to the site.</p>
+    </div>
+  );
+}
+
+export default Home;
+
+
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,65 @@
+
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+
+const navigate = useNavigate();
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (email === "web215user@gmail.com" && password === "1234") {
+        navigate("/home");
+    } else {
+        alert("Invalid credentials");
+    }
+}
+
+
+return (
+    <div className="min-h-screen  bg-gray-900 text-white">
+      <div className=" bg-white p-8 flex flex-col items-center text-black font-mono text-2xl">  {/*This is a comment, this is the top white bar */}
+        <p className="text-black">Currently Building.....</p>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-40">
+
+      <form onSubmit={handleLogin} className="flex flex-col  bg-white w-1/3  p-5 rounded-lg text-black 
+       text-2xl shadow-[0_0_30px_rgba(255,255,255,0.2)]">
+
+            <h2 className="text-2xl font-bold text-black mb-10 font-bold text-center">Login</h2>
+            <div className="flex flex-col space-y-1 font-mono">
+            <label className="text-black mb-1 ">
+                Email
+            </label>
+            <input type="email" className="p-2 rounded bg-gray-700 text-white outline-none mb-10 w-full " placeholder='sma@gmail.com'
+            value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className="flex flex-col space-y-1 font-mono">
+            <label className="text-black mb-1">
+                Password
+            </label>
+            <input type="password" className="p-2 rounded bg-gray-700 text-white outline-none mb-10 w-full " placeholder='password' 
+            value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div className="flex justify-center">
+            <button type="submit" className="w-40 bg-gray-700 hover:bg-red-700 text-white font-medium p-3 rounded-lg transition mb-10">
+                Login
+            </button>
+            </div>
+             <div className="text-sm flex flex-col border-red-500 border-1 rounded-lg w-1/3 mx-auto">
+                <p >Email: web215user@gmail.com</p>
+                <p>Pass: 1234</p>
+            </div>
+      </form>
+
+      </div>
+      </div>
+  );
+
+}
+
+export default Login;
